df27060b8195f55b37ab92e9e851d42528513102
Silvia Zhdanska <silviaalbert1992@gmail.com>
2024-09-29 21:48:36 +0300

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React from "react";
-import { Provider } from "react-redux";
+import React, { useEffect } from "react";
+import { Provider, useDispatch } from "react-redux";
 import store from "./redux/store";
 import ContactForm from "./components/contactForm/ContactForm";
 import ContactList from "./components/contactList/ContactList";
 import SearchBox from "./components/searchBox/SearchBox";
+import { fetchContacts } from "./redux/contactsSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <div>
       <h1>Contact Book</h1>
@@ -16,4 +23,10 @@ function App() {
   );
 }
 
-export default App;
+export default function Root() {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+}
